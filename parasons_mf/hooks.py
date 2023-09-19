e5759503045df4feda1f1da224aa150a39e94637
rohitkumar8848 <rohitkumar@8848digital.com>
2023-09-19 10:25:59 +0000

    Transfered script to frontend

--- parasons_mf/hooks.py
+++ parasons_mf/hooks.py
@@ -29,7 +29,7 @@ app_license = "MIT"
 # page_js = {"page" : "public/js/file.js"}
 
 # include js in doctype views
-doctype_js = {"Work Order" : "customizations/work_order/work_order.js"}
+# doctype_js = {"Work Order" : "customizations/work_order/work_order.js"}
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
